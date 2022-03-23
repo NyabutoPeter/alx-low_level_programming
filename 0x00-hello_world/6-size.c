c53a0d52f47d5adbf6b76612907986b25bd14bc6
NyabutoPeter <Nyabuto.dev@gmail.com>
2022-03-23 06:12:38 -0700

    corrected 4

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,7 +14,7 @@ long long int llT;
 float floatType;
 
 printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(charType));
-printf("size of a int: %lu byte(s)\n", (unsigned long)sizeof(intType));
+printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(intType));
 printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(lType));
 printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(llT));
 printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(floatType));
